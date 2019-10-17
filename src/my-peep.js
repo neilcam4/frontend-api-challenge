c1add4d0b6adca697e2cc9a420336133b33319c9
Neil Campbell <neildcampbell@hotmail.com>
2019-10-17 06:13:29 +0100

    view one peep

--- src/my-peep.js
+++ src/my-peep.js
@@ -1,24 +1,21 @@
-
 (function(exports){
     function getMyPeep(){
         var num = document.getElementById("number").value
         console.log(num)
-            fetch("https://chitter-backend-api.herokuapp.com/peeps/" + num)
+            fetch("https://chitter-backend-api.herokuapp.com/peeps")
             .then(function(result){
                 return result.json()
             })
             .then(function(data){
                 let output = '';
-                const response = data;
-                console.log(data)
-                response.forEach(function(peep){  
-                 output+=`<div class="alert alert-info" role="alert">${peep.body}</div>`;
-                });
-                    document.querySelector('#onePeep').innerHTML = output;
+                const peep = data;
+                console.log(data) 
+                output+=`<div class="alert alert-info" role="alert">${peep[num].body}</div>`;
+                document.querySelector('#onePeep').innerHTML = output;
                 })
-            .catch( function errorCall(e){
+            .catch(function errorCall(e){
                 console.log("error" , e)}
-                )     
+                ) 
             }
     exports.getMyPeep = getMyPeep;
 })(this)
